6ffcbd309bf582b3556a9dfeb5a44bc3f3275d57
miken-hg5 <54885677+miken-hg5@users.noreply.github.com>
2020-01-29 17:41:48 +0000

    go to about us

--- cypress/integration/cefetra/home_page.js
+++ cypress/integration/cefetra/home_page.js
@@ -1,6 +1,6 @@
 describe('Launch Home Page', function() {
   it('Visits the Home Page', function() {
     //cy.viewport(1600, 1200)
-    cy.visit('https://cefetra.co.uk')
+    cy.visit('https://cefetra.co.uk/about-us/')
   })
 })  
